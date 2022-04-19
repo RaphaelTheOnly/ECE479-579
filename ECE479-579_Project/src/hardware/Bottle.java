efa5616594d5b4131e1c31aae7ce9a371cd0ac6b
westl1 <westl1@email.arizona.edu>
2022-04-18 17:56:20 -0700

    bottle class updated - still need temp control, preloaded bin

--- ECE479-579_Project/src/hardware/Bottle.java
+++ ECE479-579_Project/src/hardware/Bottle.java
@@ -1,28 +1,59 @@
 package hardware;
 
 public class Bottle {
-	private int fill;
+	// max volume is 6 gallons = 768 ounces
+	private int ouncesRemaining;
+	private boolean isEmpty;
     
-	public Bottle() {
-		fill = 100;
+	// constructor
+	Bottle() {
+		ouncesRemaining = 768;
+		isEmpty = false;
 	}
 	
-	public void fillBottle() {
-		fill = 100;
-	}
-	public void emptyBottle() {
-		fill = 0;
+	// getter for how many ounces left in bottle
+	public int getOunces() {
+		return ouncesRemaining;
 	}
 	
-	public int dispensed(int vol) {
-		fill -= vol;
-		return fill;
-	}
+	// calculate percentage fullness of bottle
 	public int howFull() {
-		return fill;
+		double percentFull = (ouncesRemaining / 768.0);
+		percentFull = percentFull * 100.0;
+		int integerPercentage = (int) percentFull; // will floor down to nearest int value
+		
+		if (integerPercentage == 0 && getOunces() > 0) {
+			integerPercentage = 1; // display 1% if there is a little bit of water left
+		}
+		
+		// TODO if = 0, isEmpty = true, replace bottle
+		
+		return integerPercentage;
 	}
-    public void printFill() {
-    	System.out.println("Bottle fill is: " + fill);
+	
+	public void dispense(int vol) {
+		ouncesRemaining -= vol;
+		if (ouncesRemaining <= 0) {
+			ouncesRemaining = 0;
+			isEmpty = true;
+		}
+	}
+	
+    public void printHowFull() {
+    	System.out.println("Bottle is " + howFull() + "% full");
     }
-        
+    
+    // local testing
+//    public static void main(String args[]) {
+//    	System.out.println("Hello");
+//    	Bottle b1 = new Bottle();
+//    	System.out.println("ounces in b1: " + b1.getOunces());
+//    	b1.dispense(100);
+//    	System.out.println("ounces in b1: " + b1.getOunces());
+//    	b1.printHowFull();
+//    	b1.dispense(968);
+//    	System.out.println("ounces in b1: " + b1.getOunces());
+//    	b1.printHowFull();
+//    	
+//    }
 }
