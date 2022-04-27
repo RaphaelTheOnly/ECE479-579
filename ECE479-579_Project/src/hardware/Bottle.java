1cc1704b7bc889cc6f9f4362e02c9b3be99e1ec9
westl1 <westl1@email.arizona.edu>
2022-04-26 17:22:46 -0700

    Updated Block World logic for RobotArm

--- ECE479-579_Project/src/hardware/Bottle.java
+++ ECE479-579_Project/src/hardware/Bottle.java
@@ -4,6 +4,8 @@ public class Bottle {
 	// max volume is 6 gallons = 768 ounces
 	private int ouncesRemaining;
 	private boolean isEmpty;
+	private boolean lessThanQuarterFull;
+	private String status;
     
 	// constructor
 	public Bottle() {
@@ -13,10 +15,24 @@ public class Bottle {
 	// constructor
 	public Bottle(int percentFull) {
 		
-		ouncesRemaining = 768 * percentFull / 100;
-		if (ouncesRemaining > 0)
-			isEmpty = false;
-		else isEmpty = true;
+		if (percentFull == -1) {
+			status = "nullBottle";
+		}
+		else {
+			ouncesRemaining = 768 * percentFull / 100;
+			if (ouncesRemaining < 192) {
+				lessThanQuarterFull = true;
+			}
+			else lessThanQuarterFull = false;
+			if (ouncesRemaining > 0) {
+				isEmpty = false;
+				status = "full";
+			}
+			else {
+				isEmpty = true;
+				status = "empty";
+			}
+		}
 	}
 		
 	// getter for how many ounces left in bottle
@@ -41,6 +57,9 @@ public class Bottle {
 	
 	public void dispense(int vol) {
 		ouncesRemaining -= vol;
+		if (ouncesRemaining < 192) {
+			lessThanQuarterFull = true;
+		}
 		if (ouncesRemaining <= 0) {
 			ouncesRemaining = 0;
 			isEmpty = true;
@@ -52,16 +71,19 @@ public class Bottle {
     }
     
     // local testing
-//    public static void main(String args[]) {
-//    	System.out.println("Hello");
-//    	Bottle b1 = new Bottle();
-//    	System.out.println("ounces in b1: " + b1.getOunces());
-//    	b1.dispense(100);
-//    	System.out.println("ounces in b1: " + b1.getOunces());
-//    	b1.printHowFull();
-//    	b1.dispense(968);
-//    	System.out.println("ounces in b1: " + b1.getOunces());
-//    	b1.printHowFull();
-//    	
-//    }
+    public static void main(String args[]) {
+    	System.out.println("Hello");
+    	Bottle b1 = new Bottle();
+    	System.out.println("ounces in b1: " + b1.getOunces());
+    	System.out.println("Status: " + b1.status);
+    	b1.dispense(100);
+    	System.out.println("ounces in b1: " + b1.getOunces());
+    	b1.printHowFull();
+    	b1.dispense(968);
+    	System.out.println("ounces in b1: " + b1.getOunces());
+    	b1.printHowFull();
+    	Bottle b2 = new Bottle(-1);
+    	System.out.println("Status of b2: " + b2.status);
+    	
+    }
 }
--- ECE479-579_Project/src/hardware/RobotArm.java
+++ ECE479-579_Project/src/hardware/RobotArm.java
@@ -5,6 +5,14 @@ public class RobotArm {
 	private String stateOfBottles[] = new String[5];
 	private int emptyBottleCounter;
 	
+	private boolean handEmpty;
+	private int armPosition;
+	private int vacantPosition;
+	private int nextVacantPosition;
+	private boolean isIdle;
+	private boolean holding;
+	private String contentsOfHolding;
+	
 	// constructor
 	RobotArm(){
 		// initial state
@@ -12,9 +20,16 @@ public class RobotArm {
 		stateOfBottles[1] = "full";
 		stateOfBottles[2] = "full";
 		stateOfBottles[3] = "full";
-		stateOfBottles[4] = "null";
+		stateOfBottles[4] = "vacant";
 		
 		emptyBottleCounter = 0;
+		handEmpty = true;
+		armPosition = 99; // idle position
+		vacantPosition = 4;
+		nextVacantPosition = 99; // intentionally set out of range
+		isIdle = true;
+		holding = false;
+		contentsOfHolding = "";
 	}
 	
 	// resets the robot arm to initial state 1 (4 full bottles)
@@ -25,27 +40,118 @@ public class RobotArm {
 		stateOfBottles[1] = "full";
 		stateOfBottles[2] = "full";
 		stateOfBottles[3] = "full";
-		stateOfBottles[4] = "null";
+		stateOfBottles[4] = "vacant";
 		
 		emptyBottleCounter = 0;
 	}
 	
-	// call this when the current bottle runs empty, replace it and rotate all the bottles accordingly
+//	// call this when the current bottle runs empty, replace it and rotate all the bottles accordingly
+//	public void rotateBottlesOld() {
+//		
+//		// [0] should be "empty" at the start of this rotation
+//		stateOfBottles[0] = "empty";
+//		
+//		// [4] should always be "null" in any initial state for this function, so we can overwrite it first
+//		stateOfBottles[4] = stateOfBottles[0];
+//		stateOfBottles[0] = stateOfBottles[1];
+//		stateOfBottles[1] = stateOfBottles[2];
+//		stateOfBottles[2] = stateOfBottles[3];
+//		stateOfBottles[3] = stateOfBottles[4]; // which has been overwritten with empty [0] already
+//											  // so [3] should be empty now
+//		stateOfBottles[4] = "vacant";
+//		
+//		emptyBottleCounter ++;
+//	}
+	
 	public void rotateBottles() {
 		
-		// [0] should be "empty" at the start of this rotation
 		stateOfBottles[0] = "empty";
 		
-		// [4] should always be "null" in any initial state for this function, so we can overwrite it first
-		stateOfBottles[4] = stateOfBottles[0];
-		stateOfBottles[0] = stateOfBottles[1];
-		stateOfBottles[1] = stateOfBottles[2];
-		stateOfBottles[2] = stateOfBottles[3];
-		stateOfBottles[3] = stateOfBottles[4]; // which has been overwritten with empty [0] already
-											  // so [3] should be empty now
-		stateOfBottles[4] = "null";
-		
+		if (isIdle) {
+			isIdle = false;
+			while (!isIdle) {
+				moveArm(0);
+				if (vacantPosition != 0 && handEmpty && armPosition == 0) {
+					pickupBottle(0);
+				}
+				moveArm(4);
+				if (holding && vacantPosition == 4) {
+					placeBottle(4);
+				}
+				moveArm(1);
+				if (vacantPosition != 1 && handEmpty && armPosition == 1) {
+					pickupBottle(1);
+				}
+				moveArm(0);
+				if (holding && vacantPosition == 0) {
+					placeBottle(0); // this is where install() would happen
+				}
+				moveArm(2);
+				if (vacantPosition != 2 && handEmpty && armPosition == 2) {
+					pickupBottle(2);
+				}
+				moveArm(1);
+				if (holding && vacantPosition == 1) {
+					placeBottle(1);
+				}
+				moveArm(3);
+				if (vacantPosition != 3 && handEmpty && armPosition == 3) {
+					pickupBottle(3);
+				}
+				moveArm(2);
+				if (holding && vacantPosition == 2) {
+					placeBottle(2);
+				}
+				moveArm(4);
+				if (vacantPosition != 4 && handEmpty && armPosition == 4) {
+					pickupBottle(4);
+				}
+				moveArm(3);
+				if (holding && vacantPosition == 3) {
+					placeBottle(3);
+				}
+				isIdle = true;
+			}
+		}
+		armPosition = 99; // reset to idle position
+		stateOfBottles[4] = "vacant"; // reset to proper condition
 		emptyBottleCounter ++;
+	}
+	
+	// move arm TO position x [0 - 4]
+	// Block world logic:
+	// P & D: can be called for all states
+	// A: armPosition(x)
+	public void moveArm(int x) {
+		armPosition = x;
+	}
+	
+	// Block world logic:
+	// P & D: ~vacant(x) && handEmpty && armPosition(x)
+	// A: holding, contentsOfHolding(x), vacant(x)
+	public void pickupBottle(int x) {
+		// add list
+		holding = true;
+		contentsOfHolding = stateOfBottles[x];
+		nextVacantPosition = x; // don't want to overwrite the vacant position until bottle is placed.
+								// - while bottle is held, there are 2 vacant positions.
+		// delete list
+		handEmpty = false;
+	}
+	
+	// Block world logic:
+	// P & D: holding && vacant(x)
+	// A: handEmpty, contentsAssigned(x)
+	public void placeBottle(int x) {
+		//add list
+		handEmpty = true;
+		stateOfBottles[x]= contentsOfHolding; 
+		
+		// delete list
+		vacantPosition = nextVacantPosition;
+		nextVacantPosition = 99; // reset to out of range
+		holding = false;
+		contentsOfHolding = "";
 	}
 	
 	public void printStateArray() {
@@ -77,7 +183,8 @@ public class RobotArm {
 //		r1.technicianRestock();
 //		r1.printStateArray();
 //		System.out.println(r1.emptyBottleCounter);
-//		Bottle b2 = new Bottle();
+//		System.out.println("contents of hold: " + r1.contentsOfHolding);
+//		//Bottle b2 = new Bottle();
 //		
 //	}
 }
